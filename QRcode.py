e146a9330d79f644bec307eec336dcfc132d11a6
klhong124 <klhong124@gmail.com>
2018-08-29 17:06:03 +0800

    init

---
+++ QRcode.py
@@ -0,0 +1,12 @@
+import pyqrcode
+
+genQR = pyqrcode.create("Hello World")
+genQR.png('newQR.png',scale=5)
+
+#from pdf2jpg import pdf2jpg
+#result = pdf2jpg.convert_pdf2jpg("pdfQR.pdf","QRfolder",pages="1")
+
+from pyzbar.pyzbar import decode
+from PIL import Image
+result = decode(Image.open("newQR.png"))
+print(str(result[0].data)[2:-1])

c75d02807a35c2e601d1c042038d6af44a444763
klhong124 <klhong124@gmail.com>
2018-08-29 17:49:32 +0800

    NWCON ver

--- QRcode.py
+++ QRcode.py
@@ -8,5 +8,7 @@ genQR.png('newQR.png',scale=5)
 
 from pyzbar.pyzbar import decode
 from PIL import Image
-result = decode(Image.open("newQR.png"))
-print(str(result[0].data)[2:-1])
+#result = decode(Image.open("QRfolder//allQR.pdf//1_allQR.pdf.jpg",'r'))
+result = decode(Image.open("TEST.jpg"))
+list = str(result[0].data).replace("\\r","").replace("\\n\\n","")[2:-1].split("\\n")
+print(list)

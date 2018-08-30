256b163703156c3de988f5b056425f1e573866b8
klhong124 <klhong124@gmail.com>
2018-08-30 17:18:08 +0800

    update for nwcon, openCV bug!

--- QRcode.py
+++ QRcode.py
@@ -1,14 +1,19 @@
 import pyqrcode
 
-genQR = pyqrcode.create("This is a message embed in QR-code")
-genQR.png('newQR.png',scale=5)
+genQR = pyqrcode.create("This is a message embed in QR-code") #CREATE QRCODE
+genQR.png('newQR.png',scale=5) #CREARE PNG
 
+##CONVERT PDF TO JPG
 #from pdf2jpg import pdf2jpg
 #result = pdf2jpg.convert_pdf2jpg("pdfQR.pdf","QRfolder",pages="1")
 
-from pyzbar.pyzbar import decode
-from PIL import Image
+from pyzbar.pyzbar import decode #TO DECODE
+from PIL import Image #TO READ IMG
+
+## FOR PDF
 #result = decode(Image.open("QRfolder//allQR.pdf//1_allQR.pdf.jpg",'r'))
-result = decode(Image.open("TEST.jpg"))
-list = str(result[0].data).replace("\\r","").replace("\\n\\n","")[2:-1].split("\\n")
-print(list)
+
+#READ QRCODE
+result = decode(Image.open("newQR.png"))
+#list = str(result[0].data).replace("\\r","").replace("\\n\\n","")[2:-1].split("\\n") #REMOVE UNNECESSARY PART(FOR NWCOM)
+print(str(result[0].data)[2:-1]) #PRINT `resukt`
